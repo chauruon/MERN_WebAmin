b4fe0cef98010a9a885a29a82de7def9828d6d28
chauruon <chauruon.lci@gmail.com>
2022-09-14 17:47:12 +0700

    chagne library

--- src/App.js
+++ src/App.js
@@ -1,39 +1,29 @@
 import React from 'react'
-import {
-    Container,
-    AppBar,
-    Typography,
-    Grow,
-    Grid,
-} from "@material-ui/core"
 
 import Posts from './component/Posts/Posts';
 import Form from './component/Form/Form';
-import useStyles from './styles';
+// import useStyles from './styles';
+
+import {
+    AppBar,
+    Container,
+    Typography,
+    Box,
+    Grid,
+    Button,
+} from "@mui/material"
 
 import memories from "../src/images/memories.jpg"
 
 const App = () => {
-    const classes = useStyles(); 
+    // const classes = useStyles(); 
     return(
-        <Container maxWidth={'lg'}>
-            <AppBar className={classes.appBar} position='static' color='inherit'>
-                <Typography className={classes.heading} variant='h2' align='center'>Memories</Typography>
-                <img className={classes.image} src={memories} alt="memories" height="60"/>
-            </AppBar>
-            <Grow in>
-                <Container>
-                    <Grid container justifyContent='space-between' alignItems='stretch' spacing={4}>
-                        <Grid item xs={12} sm={7}>
-                            <Posts/>
-                        </Grid>
-                        <Grid item xs={12} sm={5}>
-                            <Form/>
-                        </Grid>
-                    </Grid>
-                </Container>
-            </Grow>
-        </Container>
+        <AppBar sx={{ bgcolor: "unset" }}>
+            <Grid>
+                {/* <h1>SDFKLH</h1> */}
+                DFSD
+            </Grid>
+        </AppBar>
     )
 }
 
