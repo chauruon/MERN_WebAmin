fc06215d6c27b0b9aba3045281b32242f513ee07
chauruon <chauruon.lci@gmail.com>
2022-09-15 18:59:18 +0700

    asdfdsf

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import Nav from 'react-bootstrap/Nav';
 import Posts from './component/Posts/Posts';
 import Form from './component/Form/Form';
 // import useStyles from './styles';
@@ -10,7 +10,10 @@ import {
     Typography,
     Box,
     Grid,
+    GridSpacing,
+    Grow,
     Button,
+    Link
 } from "@mui/material"
 
 import memories from "../src/images/memories.jpg"
@@ -18,12 +21,56 @@ import memories from "../src/images/memories.jpg"
 const App = () => {
     // const classes = useStyles(); 
     return(
-        <AppBar sx={{ bgcolor: "unset" }}>
-            <Grid>
-                {/* <h1>SDFKLH</h1> */}
-                DFSD
-            </Grid>
-        </AppBar>
+        <Container style={{
+            // backgroundColor:'red',
+        }}> 
+            <Box 
+                p={2}
+                sx={{
+                    position:"static",
+                    height: 50,
+                    bgcolor: "blueviolet",
+                    width: "100%",
+                    display:"flex",
+                    
+                    alignItems:"center",
+                }}>
+                    <Grid
+                        sx={{
+                            bgcolor:"#fccb00",
+                            // padding: 2,
+                            display:"flex",
+                            justifyContent : "space-between",
+                        
+                        }}
+                    >
+                        <Link
+                            sx={{
+                                display:"flex",
+                                justifyContent : "space-between",
+                            }}
+                            underline='none'
+                            component="button"
+                            variant="body2"
+                            onClick={() => {
+                                console.info("I'm a button.");
+                            }}
+                            >
+                            Button 1
+                        </Link>
+                        <Link
+                            underline='none'
+                            component="button"
+                            variant="body2"
+                            onClick={() => {
+                                console.info("I'm a button.");
+                            }}
+                            >
+                            Button 2
+                            </Link>
+                    </Grid>
+            </Box>
+        </Container>
     )
 }
 
